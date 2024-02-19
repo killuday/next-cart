839df692678732ef7c75741aa861e4092168840c
Uday Bhanu <bhanusharm122@gmail.com>
2024-02-20 00:44:43 +0530

    first commit

---
+++ src/Redux/Provider.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Provider.propTypes = {
+    
+};
+
+function Provider(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Provider;
